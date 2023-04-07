4d1fd3bbb566bb10677ee314b520adaab59da879
Ali Afzal <afzal.ali.alld@gmail.com>
2023-04-07 06:50:43 +0200

    uploaded lab-6

---
+++ lab-6/Numbers/Numbers/Infile.cs
@@ -0,0 +1,61 @@
+﻿using Numbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFile;
+
+namespace Numbers
+{
+
+    public struct Occurrence
+    {
+        public int number;
+        public int count;
+        public override string ToString()
+        {
+            return "(" + number.ToString() + " " + count.ToString()+")";
+        }
+    }
+
+    public class Infile
+    {
+        private enum Status { abnorm, norm };
+
+        private TextFileReader _x;
+        private int _dx;
+        private Status _sx;
+        Occurrence _cur;
+        bool _end;
+
+        public Infile(string fileName)
+        {
+            _x = new TextFileReader(fileName);
+        }
+
+        private void Read()
+        {
+            _sx = (_x.ReadInt(out _dx)) ? Status.norm : Status.abnorm;
+        }
+
+        public void First() { Read(); Next(); }
+        public Occurrence Current() { return _cur; }
+        public bool End() { return _end; }
+        public void Next()
+        {
+            if (!(_end = (_sx == Status.abnorm)))
+            {
+                _cur.number = _dx;
+                _cur.count = 0;
+                while(Status.norm == _sx && _dx == _cur.number)
+                {
+                    ++_cur.count;
+                    Read();
+                }
+            }
+        }
+
+    }
+
+}
---
+++ lab-6/Numbers/Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Number of occurrences of numbers ---\n\n");
+            bool fileError = true;
+            do
+            {
+                try
+                {
+                    //Console.Write("File's name: ");
+                    string fileName = "input.txt";
+                    //fileName = Console.ReadLine();
+                    Infile t = new(fileName);
+
+                    for (t.First(); !t.End(); t.Next())
+                    {
+                        Console.WriteLine($"Number: {t.Current().number} occurrence: {t.Current().count}");
+                    }
+                    fileError = false;
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    Console.WriteLine("File not found!\n\n");
+                }
+            } while (fileError);
+
+        }
+    }
+}
